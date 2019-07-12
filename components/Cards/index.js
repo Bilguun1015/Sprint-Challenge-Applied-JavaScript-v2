fc0621be2f9bbd54e16701bc17c87766d5e3df9e
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-07-12 13:04:09 -0500

    still working on the funcionality

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,83 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardsContainer = document.querySelector('.cards-container')
+
+
+    axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then(data=>{
+        const articles = data.data.articles;
+        const bootstrap = articles.bootstrap;
+        const javascript = articles.javascript;
+        const jquery = articles.jquery;
+        const node = articles.node;
+        const technology = articles.technology;
+
+        
+        bootstrap.forEach(card => {
+          
+            const element = createArticleCard(card);
+            cardsContainer.appendChild(element);
+        });
+
+        javascript.forEach(card => {
+      
+            const element = createArticleCard(card);
+            cardsContainer.appendChild(element);
+        });
+        
+        jquery.forEach(card => {
+        
+            const element = createArticleCard(card);
+            cardsContainer.appendChild(element);
+        });
+
+        jquery.forEach(card => {
+            
+            const element = createArticleCard(card);
+            cardsContainer.appendChild(element);
+        });
+
+        node.forEach(card => {
+         
+            const element = createArticleCard(card);
+            cardsContainer.appendChild(element);
+        });
+
+        technology.forEach(card => {
+           
+            const element = createArticleCard(card);
+            cardsContainer.appendChild(element);
+        });
+    })
+    .catch(error=>{
+        console.log('The lambda backend is currently not working',error)
+    })
+
+    
+
+function createArticleCard (data){
+    const articleCard = document.createElement('div');
+    const articleHeadline = document.createElement('div');
+    const articleAuthor = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const nameAuthor = document.createElement('span');
+
+    articleCard.classList.add('card');
+    articleHeadline.classList.add('headline');
+    articleAuthor.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    articleHeadline.textContent = data.headline;
+    img.src = data.authorPhoto;
+    nameAuthor.textContent = data.authorName;
+
+    articleCard.appendChild(articleHeadline);
+    articleCard.appendChild(articleAuthor);
+    articleAuthor.appendChild(imgContainer);
+    articleAuthor.appendChild(nameAuthor);
+    imgContainer.appendChild(img);
+
+    return articleCard;
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -11,6 +11,7 @@ const topics = document.querySelector('.topics');
 axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
     .then(data=>{
         const dataLambda = data.data.topics;
+        console.log(dataLambda)
         dataLambda.forEach(item => {
             const element = createTab(item);
             topics.appendChild(element)
@@ -21,9 +22,32 @@ axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
     })
 
 
+
 function createTab(array){
    const tab = document.createElement('div');
    tab.classList.add('tab');
    tab.textContent = array;
+
+   tab.addEventListener('click', event=>{
+       console.log('button clicked', event.target);
+       cardstoShow = document.querySelectorAll('.card')
+       cardstoShow.style.display = 'none';
+   })
+
    return tab;
 }
+
+function createAllTab(){
+    const tabAll = document.createElement('div');
+    tabAll.classList.add('tab');
+    tabAll.textContent = 'All';
+
+    tabAll.addEventListener('click',event=>{
+
+    })
+
+    return tabAll;
+
+    
+}
+topics.appendChild(createAllTab());
