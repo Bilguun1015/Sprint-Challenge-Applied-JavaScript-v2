035da2cdebcfdf2a3070d2cdadd8e60009482aa5
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-07-12 11:24:52 -0500

    finished header.js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,23 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+const topics = document.querySelector('.topics'); 
+axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
+    .then(data=>{
+        const dataLambda = data.data.topics;
+        dataLambda.forEach(item => {
+            const element = createTab(item);
+            topics.appendChild(element)
+        });  
+    })
+    .catch(error=>{
+        console.log('The lambda backend is currently not working',error)
+    })
+
+
+function createTab(array){
+   const tab = document.createElement('div');
+   tab.classList.add('tab');
+   tab.textContent = array;
+   return tab;
+}
